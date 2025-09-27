aab16ed26478f9c9c160b02b656edbaa89a3f163
junhong.lee429@gmail.com <junhong.lee429@gmail.com>
2025-09-27 09:45:54 +0900

    Orbit controls

---
+++ 06-cameras/src/script.js
@@ -0,0 +1,95 @@
+import * as THREE from 'three'
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
+
+/**
+ * Cursor
+ */
+const cursor = {
+    x: 0,
+    y: 0
+}
+window.addEventListener('mousemove', (event) => {
+    cursor.x = event.clientX / sizes.width - 0.5
+    cursor.y = -(event.clientY / sizes.height - 0.5)
+})
+
+/**
+ * Base
+ */
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
+
+// Sizes
+const sizes = {
+    width: 800,
+    height: 600
+}
+
+// Scene
+const scene = new THREE.Scene()
+
+// Object
+const mesh = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1, 5, 5, 5),
+    new THREE.MeshBasicMaterial({ color: 0xff0000 })
+)
+scene.add(mesh)
+
+// Camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
+
+// const aspectRatio = sizes.width / sizes.height
+
+// const camera = new THREE.OrthographicCamera(
+//     -1 * aspectRatio, 
+//     1 * aspectRatio, 
+//     1, 
+//     -1
+// )
+
+// camera.position.x = 2
+// camera.position.y = 2
+camera.position.z = 2
+camera.lookAt(mesh.position)
+scene.add(camera)
+
+// Controls
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
+// controls.target.y = 1
+// controls.update()
+
+// Renderer
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+})
+renderer.setSize(sizes.width, sizes.height)
+
+// Animate
+const clock = new THREE.Clock()
+
+const tick = () =>
+{
+    const elapsedTime = clock.getElapsedTime()
+
+    // Update objects
+    // mesh.rotation.y = elapsedTime;
+
+    // Update camera - 원형으로 회전하면서 mesh를 바라보기
+    // const radius = 5
+    // camera.position.x = Math.cos(cursor.x * Math.PI * 2) * radius
+    // camera.position.z = Math.sin(cursor.x * Math.PI * 2) * radius
+    // camera.position.y = cursor.y * radius
+    // camera.lookAt(mesh.position)
+
+    // Update controls
+    controls.update()
+
+    // Render
+    renderer.render(scene, camera)
+
+    // Call tick again on the next frame
+    window.requestAnimationFrame(tick)
+}
+
+tick()
